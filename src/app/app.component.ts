97ba4c7fc30f2fcaaaa20d23a2a5ebd40bb0bd16
Rodriguez Chaparro <diego.rodriguez_chaparro@siemens.com>
2019-03-27 08:48:27 -0500

    Adding first files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'esriTest';
+  // Set our map properties
+  mapCenter = [-74.183888, 4.777068];
+  basemapType = 'topo';
+  mapZoomLevel = 16;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log('The map loaded: ' + status);
+  }
 }

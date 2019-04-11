3c384b56abbb1e6c245ef0dff6bd75bf8fc30d09
Diego R <diefrodriguezcha@unal.edu.co>
2019-04-11 10:15:34 -0500

    Perfil con datos (Falta cambiar fechaNacimiento)

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -9,24 +9,13 @@ import { DataRetrieverService } from '../data-retriever.service';
 export class PerfilComponent implements OnInit {
 
   infoEspecialista={};
-  tecnica;
+  fechaNacimiento;
   constructor(private DataRetriever: DataRetrieverService) { }
 
   ngOnInit() {
     this.DataRetriever.infoEspecialista.subscribe(infoEspecialista => this.infoEspecialista = infoEspecialista);
-    console.log(this.infoEspecialista);
-    switch(this.infoEspecialista['IdTecnica']) {
-      case 1:
-        this.tecnica = 'VFD';
-      case 2:
-        this.tecnica = 'BT';
-      case 3:
-        this.tecnica = 'AUT';
-      case 4:
-        this.tecnica = 'AOS';
-      case 5:
-        this.tecnica = 'MOT';
-    } 
+    
+    this.fechaNacimiento=this.infoEspecialista['FechaNacimiento'].split("T")[0];
   }
 
 }

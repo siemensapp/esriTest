8bdc8bb2313e32f1e93bd97afa32d609f9e23607
Diego R <diefrodriguezcha@unal.edu.co>
2019-04-22 11:38:50 -0500

    Creando los filtros para la tabla

--- src/app/asignaciones-eliminadas/asignaciones-eliminadas.component.ts
+++ src/app/asignaciones-eliminadas/asignaciones-eliminadas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as env from '../../assets/variables';
+import { DataRetrieverService } from '../data-retriever.service';
+import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-asignaciones-eliminadas',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsignacionesEliminadasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataRetriever : DataRetrieverService) { }
 
+  resultados: JSON[];
+
+  agregarListener(){
+    document.getElementById('fecha').addEventListener("change", (event) => {
+      var fecha = document.getElementById('fecha').attributes[2].ownerDocument.activeElement.value;
+      var texto = document.getElementById('buscar').attributes[2].ownerElement.value;
+      if(isUndefined(fecha)){
+        fecha = '';
+      }
+      this.dataRetriever.getData(env.url+'/api/getDeletedAssignments/'+fecha+'/'+texto).then(data => {
+        this.resultados = data as JSON[];
+        console.log(this.resultados);
+        })
+
+    });
+
+    document.getElementById('buscar').addEventListener("change", (event) => {
+      var fecha = document.getElementById('fecha').attributes[2].ownerDocument.activeElement.value;
+      var texto = document.getElementById('buscar').attributes[2].ownerElement.value;
+      if(isUndefined(fecha)){
+        fecha = '';
+      }
+      this.dataRetriever.getData(env.url+'/api/getDeletedAssignments/'+fecha+'/'+texto).then(data => {
+        this.resultados = data as JSON[];
+        console.log(this.resultados);
+        })
+    });
+
+  }
+  traerAsignacionesEliminadas(){
+    this.dataRetriever.getData(env.url+"/api/getDeletedAssignments/''/''").then(data => {
+      this.resultados = data as JSON[];
+      console.log(this.resultados);
+      })
+  }
   ngOnInit() {
+    console.log(document.getElementById(''));
+       this.traerAsignacionesEliminadas();
+       this.agregarListener();
   }
 
 }
